f53fea2ffba1a50b92393613c91c9bc2e5397fe3
Megajin <Gueney.Oezdemir@gmx.de>
2021-05-08 19:45:09 +0200

    ✨ added new helpers and relevant tests

--- __test__/helpers.test.js
+++ __test__/helpers.test.js
@@ -1,4 +1,13 @@
-const { transformPageID, capitalizeFirstLetter, normalizePathToPageName } = require('../src/helpers');
+/* eslint no-undef: 0 */
+
+const {
+  transformPageID,
+  capitalizeFirstLetter,
+  normalizePathToPageName,
+  capitalizeFirstLetterInAllWords,
+  transformAllWhitespaceWithMinus,
+  transformUmlauts
+} = require('../src/helpers');
 
 test('transformPageID check', () => {
   expect(transformPageID('SitePage /about-us/')).toMatch(/PageTree-about-us-id-[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}/i);
@@ -10,4 +19,25 @@ test('capitalizeFirstLetter check', () => {
 
 test('normalizePathToPageName check', () => {
   expect(normalizePathToPageName('/about-us/')).toBe('About us');
+});
+
+test('capitalizeFirstLetterInAllWords check', () => {
+  expect(capitalizeFirstLetterInAllWords('about us')).toBe('About Us');
+  expect(capitalizeFirstLetterInAllWords('about-us')).toBe('About-us');
+  expect(capitalizeFirstLetterInAllWords('about - us')).toBe('About - Us');
+});
+
+test('transformAllWhitespaceWithMinus check', () => {
+  expect(transformAllWhitespaceWithMinus('/about us     1/')).toBe('/about-us-1/');
+  expect(transformAllWhitespaceWithMinus('/about us/')).toBe('/about-us/');
+  expect(transformAllWhitespaceWithMinus('/about  us  test     test/')).toBe('/about-us-test-test/');
+});
+
+test('transformUmlauts check', () => {
+  expect(transformUmlauts('/über uns/')).toBe('/ueber uns/');
+  expect(transformUmlauts('/ueber uns/', true)).toBe('/über uns/');
+  expect(transformUmlauts('/Über uns/')).toBe('/UEber uns/');
+  expect(transformUmlauts('/UEber uns/', true)).toBe('/Über uns/');
+  expect(transformUmlauts('/Grüße von mir/')).toBe('/Gruesse von mir/');
+  expect(transformUmlauts('/Gruesse von mir/', true)).toBe('/Grüsse von mir/');
 });
--- src/helpers.js
+++ src/helpers.js
@@ -23,6 +23,68 @@ function capitalizeFirstLetter([first, ...rest], locale) {
 }
 
 /**
+ * Capitalizes first letters of words in string.
+ * @param {string} str String to be modified
+ * @param {boolean=false} lower Whether all other letters should be lowercased
+ * @return {string}
+ * @see https://stackoverflow.com/questions/2332811/capitalize-words-in-string/7592235#7592235
+ * @usage
+ *   capitalize('fix this string');     // -> 'Fix This String'
+ *   capitalize('javaSCrIPT');          // -> 'JavaSCrIPT'
+ *   capitalize('javaSCrIPT', true);    // -> 'Javascript'
+ */
+function capitalizeFirstLetterInAllWords(str, lower = false) {
+  return (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) => match.toUpperCase());
+}
+
+/**
+ * Replaces all Whitespaces in String with minus.
+ * @param  {string} str String to be modified
+ * @return {string}
+ * @usage
+ *   transformAllWhitespaceWithMinus('hello world');     // -> 'hello-world'
+ *   transformAllWhitespaceWithMinus('Hello World   1');          // -> 'Hello-World-1'
+ */
+function transformAllWhitespaceWithMinus(str) {
+  return str.replace(/\s\s+/g, ' ').replace(/\s/g, '-');
+}
+
+// German Umlauts transform to URL Safe (better for SEO).
+const _umlautsMap = [
+  { code: '\u00C4', value: 'Ä', safe: 'AE' },
+  { code: '\u00E4', value: 'ä', safe: 'ae' },
+  { code: '\u00D6', value: 'Ö', safe: 'OE' },
+  { code: '\u00F6', value: 'ö', safe: 'oe' },
+  { code: '\u00DC', value: 'Ü', safe: 'UE' },
+  { code: '\u00FC', value: 'ü', safe: 'ue' },
+  { code: '\u00DF', value: 'ß', safe: 'ss' }
+];
+
+const _unicodeToChar = (text) => text.replace(/\\u[\dA-F]{4}/gi, (match) => String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16)));
+const _umlaute = _umlautsMap.map((u) => [u.safe, _unicodeToChar(u.code)]);
+
+/**
+ * Replaces all German Umlauts with SEO safe values.
+ * @param  {string} str String to be modified
+ * @return {string}
+ * @usage
+ *   transformUmlauts('Grüße von mir);     // -> 'Gruesse von mir'
+ *   transformUmlauts('Ölspuren sind gefährlich');          // -> 'Oelspuren sind gefaehrlich'
+ *   transformUmlauts('Gruesse von mir', true);          // -> 'Grüße von mir'
+ */
+function transformUmlauts(str, restore = false) {
+  for (let u = 0; u < _umlaute.length; u++) {
+    if (restore) {
+      // there is some unwanted behaviour in restore with double s.
+      str = _umlaute[u][0] !== 'ss' ? str.replace(_umlaute[u][0], _umlaute[u][1]) : str;
+    } else {
+      str = str.replace(_umlaute[u][1], _umlaute[u][0]);
+    }
+  }
+  return str;
+}
+
+/**
  * Takes the SitePage node path and outputs a readable name.
  * @param  {String} str given path e.g. /about-us/
  * @return {String}     readable string e.g. about us.
@@ -38,5 +100,8 @@ function normalizePathToPageName(str) {
 module.exports = {
   transformPageID,
   capitalizeFirstLetter,
-  normalizePathToPageName
+  normalizePathToPageName,
+  capitalizeFirstLetterInAllWords,
+  transformAllWhitespaceWithMinus,
+  transformUmlauts
 };
